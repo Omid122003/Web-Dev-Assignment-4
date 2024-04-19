ed30338aa10a46682ae93821b3a4db272abfe213
Omid <146298465+Omid122003@users.noreply.github.com>
2024-04-19 18:39:32 -0400

    Update main.js

--- Part-2/gallery-start/main.js
+++ Part-2/gallery-start/main.js
@@ -5,7 +5,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const alts = {
+  'pic1.jpg' : 'Closeup of a human eye',
+  'pic2.jpg' : 'Rock that looks like a wave',
+  'pic3.jpg' : 'Purple and white pansies',
+  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
+  'pic5.jpg' : 'Large moth on a leaf'
+}
 
 /* Looping through images */
 for (let i = 0; i < imageFilenames.length; i++) {
@@ -34,3 +41,4 @@ btn.addEventListener('click', function(){});
     btn.textContent = 'Darken';
     overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
   }
+  
